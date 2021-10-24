a5d5d2df444e43a95475c539cef3159e56cc406f
thiago <thiagohcalvi@gmail.com>
2021-10-24 01:23:12 -0300

    24/10

---
+++ migrations/20211024031908-create-item-pedido.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ItemPedidos', {
+      PedidoId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Pedidos',
+          key: 'Id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      ServicoId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Servicos',
+          key: 'Id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      quantidade: {
+        type: Sequelize.INTEGER
+      },
+      valor: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ItemPedidos');
+  }
+};
